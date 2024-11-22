894da30b1ad45fbda3be915f424792ccfa2e4f09
Samuel Plaza <samuelplazasaez@gmail.com>
2024-11-22 09:54:28 +0100

    v1.0.6 fining bugs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,5 @@
 // import { useEffect } from 'react'
 import '../css/Login.css'
-import '../App.css'
 import { createClient } from "@supabase/supabase-js"
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
