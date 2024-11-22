0217e173e89d854be2c09f7ae5451c7e547a0776
Samuel Plaza <samuelplazasaez@gmail.com>
2024-11-22 09:05:55 +0100

    v1.0.1 fixing dependecies

--- src/components/Prueba.jsx
+++ src/components/Prueba.jsx
@@ -1,26 +1,26 @@
-import addNotification from 'react-push-notification';
+// import addNotification from 'react-push-notification';
 
-const Prueba = () => {
+// const Prueba = () => {
 
-    const buttonClick = () => {
-        addNotification({
-            title: 'WhatsApp - Clone',
-            subtitle: 'This is a subtitle',
-            message: 'This is a very long message',
-            theme: 'darkblue',
-            native: true, // when using native, your OS will handle theming.
-            icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/800px-WhatsApp.svg.png",
-            vibrate: [2]
-        });
-    };
+//     const buttonClick = () => {
+//         addNotification({
+//             title: 'WhatsApp - Clone',
+//             subtitle: 'This is a subtitle',
+//             message: 'This is a very long message',
+//             theme: 'darkblue',
+//             native: true, // when using native, your OS will handle theming.
+//             icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/800px-WhatsApp.svg.png",
+//             vibrate: [2]
+//         });
+//     };
 
-    return (
-      <div className="page">
-          <button onClick={buttonClick} className="button">
-           Hello world.
-          </button>
-      </div>
-    );
-  }
+//     return (
+//       <div className="page">
+//           <button onClick={buttonClick} className="button">
+//            Hello world.
+//           </button>
+//       </div>
+//     );
+//   }
 
-export default Prueba;
+// export default Prueba;
